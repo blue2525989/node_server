c6fef8c65bcc6f9c9394657fa7e78bfc30859e98
jason <jbierbrauer99@gmail.com>
2017-10-17 20:21:44 -0500

    add optional message

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const http = require("http");
 /* grab command line args, 0=node,[ 1=path/to/file.js, rest arguments */
 const port = process.argv[2];
 
+/* optional custom message */
+var message;
+if (process.argv[3] != null) {
+	message = process.argv[3];
+} else {
+	message = "Hello World!";
+}
+
 /* request and response are similar to httpServletRequest and httpServletRespone in Java? */
 function onRequest(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
+	response.write(message);
 	response.end();
 };
 
