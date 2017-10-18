9dc9f3d95b1b48c82dcbe42d9896cf1668a0747e
jason <jbierbrauer99@gmail.com>
2017-10-17 21:28:52 -0500

    added command line arguments

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 /* imports http library and assigns to http variable */
 const http = require("http");
 
+/* import readline to read from stdin */
+const readline = require("readline");
+
+/* constructor for readline module ?? */
+const readlineIO = readline.createInterface({
+	input : process.stdin,
+	output : process.stdout
+});
+
 /* grab command line args, 0=node,[ 1=path/to/file.js, rest arguments */
 const port = process.argv[2];
 
@@ -19,5 +28,34 @@ function onRequest(request, response) {
 	response.end();
 };
 
-/* pass a function as a argument */
-http.createServer(onRequest).listen(port);
+/* long string! */
+var welcome = "\nWelcome to the Simple Server.\n" +
+		"The server is now listening on port " + port + "\n" +
+		"\n" +
+		"Please type -stop to stop to stop the server.";
+
+/* terminal prompt */
+function prompt(welcome) {
+	/* readline standard input from terminal */
+	readlineIO.question(welcome, (answer) => {
+		if (answer == '-stop') {
+			console.log("Good-bye!")
+			process.exit();
+		}
+			
+		/* close IO data streams */
+		readlineIO.close();
+	});
+		
+	
+}
+
+/* start the server */
+function startServer(onRequest) {
+	/* pass a function as a argument */
+	http.createServer(onRequest).listen(port);
+}
+
+/* main loop area */
+prompt(welcome);
+startServer(onRequest);
